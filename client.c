b15e209255acc2c9f3cc0744b93540dbe93e649c
BOOO9 <caspar.conradi@posteo.org>
2021-01-05 09:43:28 +0100

    New server-client from Grü

--- client.c
+++ client.c
@@ -1,76 +1,210 @@
 /* client.c */
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
 #include <arpa/inet.h>
-#include <unistd.h>
-#include <stdlib.h>
+#include <errno.h>
+#include <netinet/in.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <pthread.h>
+
 #define BUF 1024
 
+int run = 1;
 
-void printBoard(char board[6][7])
-{
-	printf("\e[1;1H\e[2J");
+char *progname;
 
-	printf("  1 2 3 4 5 6 7\n"); 
-	printf(" ---------------\n");
+void printBoard(char board[6][7]) {
+  printf("\e[1;1H\e[2J");
 
-	for(int i=0; i < 6; i++) {
-		printf("%d", i+1);
-    		  for(int j=0; j < 7; j++) {
-        		 printf("|%c", board[i][j]);
-		   }
-		printf("|\n");
-	}
+  printf("  1 2 3 4 5 6 7\n");
+  printf(" ---------------\n");
 
-	printf(" ---------------\n\n");
+  for (int i = 0; i < 6; i++) {
+    printf("%d", i + 1);
+    for (int j = 0; j < 7; j++) {
+      printf("|%c", board[i][j]);
+    }
+    printf("|\n");
+  }
+
+  printf(" ---------------\n\n");
+}
+
+void error_exit(const char *msg) {
+  fprintf(stderr, "%s: %s\n", msg, strerror(errno));
+  exit(EXIT_FAILURE);
+}
+
+void usage() {
+  fprintf(stderr, "Usage: %s address port\n", progname);
+  exit(EXIT_FAILURE);
 }
 
 
-int main (int argc, char **argv) {
-  int create_socket;
-  char *buffer = malloc (BUF);
-  struct sockaddr_in address;
-  int size;
+void *send_mesg(void *arg)
+{
+  int server_sockfd = *((int *)arg);
 
-  int *entry;
+  FILE *server_sockfile = fdopen(server_sockfd, "r+");
+//  fputs("Hallo!!!\n", server_sockfile);
+//    fflush(server_sockfile);
+  char buffer[100];
+  char *message; // = fgets(buffer, sizeof(buffer), server_sockfile);
 
-  char gameBoard [6][7];
+//  printf("%s", message);
 
-  if( argc < 3 ){
-     printf("Usage: %s ServerAdresse  Portnmbr\n", *argv);
-     exit(EXIT_FAILURE);
+
+
+
+  while(1)
+  {
+	fgets(buffer, BUF, stdin);
+	fputs(buffer, server_sockfile);
+	fflush(server_sockfile);
+	printf("\nwrote to server: %s\n", buffer);
+
+	if(strcmp(buffer, "quit\n") == 0){
+		break;
+	}
+/*
+	fgets(buffer, BUF, server_sockfile);
+	printf("\ngot from server: %s", buffer);
+*/
+
+
+//	sleep(1);
   }
-//  printf ("\e[2J");
-  if ((create_socket=socket (AF_INET, SOCK_STREAM, 0)) > 0)
-    printf ("Socket wurde angelegt\n");
+
+  run = 0;
+
+  fclose(server_sockfile);
+
+}
+
+void *recive_mesg(void* arg)
+{
+  int server_sockfd = *((int *)arg);
+
+  FILE *server_sockfile = fdopen(server_sockfd, "r+");
+//  fputs("Hallo!!!\n", server_sockfile);
+//    fflush(server_sockfile);
+  char buffer[100];
+  char *message; // = fgets(buffer, sizeof(buffer), server_sockfile);
+
+//  printf("%s", message);
+
+
+
+
+  while(1)
+  {
+/*
+	fgets(buffer, BUF, );
+	fputs(buffer, server_sockfile);
+	fflush(server_sockfile);
+	printf("\nwrote to server: %s\n", buffer);
+
+	if(strcmp(buffer, "quit\n") == 0){
+		break;
+	}
+*/
+	fgets(buffer, BUF, server_sockfile);
+	printf("\ngot from server: %s", buffer);
+
+
+
+//	sleep(1);
+  }
+
+  run = 0;
+
+  fclose(server_sockfile);
+
+
+}
+
+
+int main(int argc, char **argv) {
+  int server_sockfd;
+  struct sockaddr_in address;
+  // int size;
+
+  // int *entry;
+
+  // char gameBoard [6][7];
+
+  if (argc < 3) {
+    usage();
+  }
+  if ((server_sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+    // printf("Socket wurde angelegt\n");
+    error_exit("socket failed");
+  }
+
   address.sin_family = AF_INET;
-  address.sin_port =atoi(argv[2]);
-  inet_aton (argv[1], &address.sin_addr);
-  if (connect ( create_socket,
-                (struct sockaddr *) &address,
-                sizeof (address)) == 0)
-    printf ("Verbindung mit dem Server (%s) hergestellt\n",
-       inet_ntoa (address.sin_addr));
-  do {
-      size = recv(create_socket, gameBoard, sizeof(gameBoard), 0);
-      if( size > 0)
-         buffer[size] = '\0';
-//      printf ("Nachricht erhalten: %s\n", buffer);
+  address.sin_port = atoi(argv[2]);
+  inet_aton(argv[1], &address.sin_addr);
+  if (connect(server_sockfd,
+              (struct sockaddr *)&address,
+              sizeof(address)) == 0) {
+    printf("Verbindung mit dem Server (%s) hergestellt\n",
+           inet_ntoa(address.sin_addr));
+  } else {
+    error_exit("connect failed");
+  }
 
-	printBoard(gameBoard);
 
-    if (strcmp (buffer, "quit\n")) {
-         printf ("select column: ");
-         fgets(buffer, BUF, stdin);
 
-//	printf("%s", entry);
+  pthread_t thread_recive_mesg;
+  if(pthread_create(&thread_recive_mesg, NULL, recive_mesg, (void *)&server_sockfd)< 0)
+  {
+	printf("problem send recive thread");
+	error_exit("msg thread failed");
+  }
 
-         send(create_socket, buffer, strlen(buffer), 0);
-       }
-  } while (strcmp (buffer, "quit\n") != 0);
-  close (create_socket);
+
+
+  pthread_t thread_send_mesg;
+  if(pthread_create(&thread_send_mesg, NULL, send_mesg, (void *)&server_sockfd)< 0)
+  {
+	printf("problem send msg thread");
+	error_exit("msg thread failed");
+  }
+
+
+  while(run);
+
+
+
+//  printf("\nConnection END\n\n");
+
+
+//  fputs("quit", server_sockfile);
+//  fflush(server_sockfile);
+
+
+
+  // do {
+  //   size = recv(create_socket, gameBoard, sizeof(gameBoard), 0);
+  //   if (size > 0)
+  //     buffer[size] = '\0';
+  //   //      printf ("Nachricht erhalten: %s\n", buffer);
+
+  //   printBoard(gameBoard);
+
+  //   if (strcmp(buffer, "quit\n")) {
+  //     printf("select column: ");
+  //     fgets(buffer, BUF, stdin);
+
+  //     //	printf("%s", entry);
+
+  //     send(create_socket, buffer, strlen(buffer), 0);
+  //   }
+  // } while (strcmp(buffer, "quit\n") != 0);
+  // close(create_socket);
   return EXIT_SUCCESS;
 }
+
--- server.c
+++ server.c
@@ -1,92 +1,212 @@
 /* server.c */
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
 #include <arpa/inet.h>
-#include <unistd.h>
-#include <stdlib.h>
+#include <errno.h>
+#include <netinet/in.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <pthread.h>
 
 #define BUF 1024
+#define MAX_USER 20
+#define MAX_GAMEROOM 5
+#define ROWS 6
+#define COLUMNS 7
+
+typedef struct{
+	int sockfd;
+	int player_nmbr;  //checkt loggin
+	FILE *client_sockfile;
+	int room; //sagt welcher gameroom
+	int player_nmbr_room; //sagt zuschauer od spieler (und ob x od 0)
+} player_t;
+
+player_t players[MAX_USER];
 
 
-void setToken(char board[6][7], char field[])
-{
-	int row = 0;
 
-	int set= atoi(field);
-	set--;
+typedef struct{
+	char gameboard[ROWS][COLUMNS];
+}gameroom_t;
 
-	while(board[row][set] == ' ')row++;
+gameroom_t gameroom[MAX_GAMEROOM];
 
-	board[row-1][set] = 'X';
+
+
+pthread_mutex_t client_mutex = PTHREAD_MUTEX_INITIALIZER;
+
+char *progname;
+int user_count;
+
+
+
+void setToken(char board[ROWS][COLUMNS], char field[]) {
+  int row = 0;
+
+  int set = atoi(field);
+  set--;
+
+  while (board[row][set] == ' ')
+    row++;
+
+  board[row - 1][set] = 'X';
 }
 
+void error_exit(const char *msg) {
+  fprintf(stderr, "%s: %s\n", msg, strerror(errno));
+  exit(EXIT_FAILURE);
+}
+
+void usage() {
+  fprintf(stderr, "Usage: %s port\n", progname);
+  exit(EXIT_FAILURE);
+}
+
+void *handle_client(void *arg) {
+  int sockfd = *((int *)arg);
+
+  int cur;
+
+  for(int i = 1; i < MAX_USER; i++)
+  {
+    if(players[i].player_nmbr < 0)
+    {
+      players[i].sockfd = sockfd;
+      players[i].player_nmbr = i;
+
+      cur = i;
+
+      break;
+    }
+ }
 
 
-int main (int argc, char **argv) {
 
- char gameBoard [6][7];
+  FILE *client_sockfile = fdopen(players[cur].sockfd , "r+");
 
-	for(int i=0; i < 6; i++) {
-    		  for(int j=0; j < 7; j++) {
-        		gameBoard [i][j] =' ';
-     		 }
-  	 }
+  players[cur].client_sockfile = fdopen(players[cur].sockfd , "r+");
 
 
-  int create_socket, new_socket;
+while(1){
+  char buffer[100];
+
+  char *message = fgets(buffer, sizeof(buffer), client_sockfile);
+
+  if (message != NULL)
+  {
+
+  if(strcmp(buffer, "quit\n") == 0){
+	break;
+  }
+
+
+    for(int i = 1; i <= MAX_USER; i++){
+//      fputs(message, players[cur].client_sockfile);
+      if(players[i].player_nmbr > 0)
+      {
+        printf("PRINT TO %d: \n", i);
+        fputs(message, players[i].client_sockfile);
+        fflush(players[i].client_sockfile);
+
+        printf("got it\n");
+      }
+    }
+  }
+
+
+  printf("\ngot from client usernmbr %d, fd %d: %s\n", cur, players[cur].player_nmbr, buffer);
+
+//  sleep(1);
+
+  }
+
+  printf("someone left\n");
+  user_count--;
+
+  players[cur].player_nmbr = -1;
+
+  fclose(players[cur].client_sockfile);
+}
+
+int start_server(int port) {
+  int server_sockfd, client_sockfd;
   socklen_t addrlen;
-  char *buffer = malloc (BUF);
   ssize_t size;
   struct sockaddr_in address;
-  const int y = 1;
+  // const int y = 1;
 
-  int* setField;
-//  printf ("\e[2J");
-if( argc < 2 ){
-     printf("Usage: %s Portnmbr\n", *argv);
-     exit(EXIT_FAILURE);
-   }
-
-
-
-  if ((create_socket=socket (AF_INET, SOCK_STREAM, 0)) > 0)
-    printf ("socket create succes\n");
-  setsockopt( create_socket, SOL_SOCKET,
-              SO_REUSEADDR, &y, sizeof(int));
+  if ((server_sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+    error_exit("socket failed");
+  }
+  const int optval = 1;
+  setsockopt(server_sockfd, SOL_SOCKET,
+             SO_REUSEADDR, &optval, sizeof(int));
   address.sin_family = AF_INET;
   address.sin_addr.s_addr = INADDR_ANY;
-  address.sin_port = atoi(argv[1]);
-  if (bind ( create_socket,
-             (struct sockaddr *) &address,
-             sizeof (address)) != 0) {
-    printf( "Der Port ist nicht frei – belegt!\n");
+  address.sin_port = port;
+  if (bind(server_sockfd,
+           (struct sockaddr *)&address,
+           sizeof(address)) != 0) {
+    error_exit("bind failed");
   }
-  listen (create_socket, 5);
-  addrlen = sizeof (struct sockaddr_in);
+  listen(server_sockfd, 5);
+  addrlen = sizeof(struct sockaddr_in);
+  // TODO
+  pthread_t thread_id;
   while (1) {
-     new_socket = accept ( create_socket,
-                           (struct sockaddr *) &address,
-                           &addrlen );
-     if (new_socket > 0)
-      printf ("Ein Client (%s) ist verbunden ...\n",
-         inet_ntoa (address.sin_addr));
-     do {
-//        printf ("Nachricht zum Versenden: ");
-//        fgets (buffer, BUF, stdin);
-        send (new_socket, gameBoard, sizeof (gameBoard), 0);
-size =        recv (new_socket, buffer, BUF-1, 0);
-        if( size > 0)
-           buffer[size] = '\0';
-        printf ("Nachricht empfangen: %s\n", buffer);
+    client_sockfd = accept(server_sockfd,
+                           (struct sockaddr *)&address,
+                           &addrlen);
+    if (client_sockfd > 0) {
+      printf("Connection (%s) established\n",
+             inet_ntoa(address.sin_addr));
+      user_count++;
 
-	setToken(gameBoard, buffer);
-
-     } while (strcmp (buffer, "quit\n") != 0);
-     close (new_socket);
+      if (pthread_create(&thread_id, NULL, handle_client, (void *)&client_sockfd) < 0) {
+        error_exit("pthread_create failed");
+      }
+    } else {
+      error_exit("accept client");
+      // TODO
+    }
   }
-  close (create_socket);
-  return EXIT_SUCCESS;
+  close(server_sockfd);
 }
+
+int main(int argc, char **argv) {
+
+  for(int i = 0; i<= MAX_USER; i++) players[i].player_nmbr = -1;
+
+
+
+  for(int i = 0; i<MAX_GAMEROOM; i++)
+  {
+    for (int j = 0; j < ROWS; j++)
+    {
+      for (int k = 0; k < COLUMNS; k++)
+      {
+        gameroom[i].gameboard[j][k] = ' ';
+      }
+    }
+  }
+
+
+//gameroom[3].gameboard[4][5] = ' ';
+
+  user_count = 0;
+  progname = argv[0];
+  if (argc < 2) {
+    usage();
+  }
+  int port = atoi(argv[1]);
+  if (port == 0) {
+    usage();
+  }
+
+
+  return start_server(port);
+}
+
